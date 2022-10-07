b178379daba2c03c903bc1bc75da54341f236b96
tenomiss <64487131+tenomiss@users.noreply.github.com>
2022-10-07 09:44:52 +0200

    Update ex04_chats_souris.py

--- ex04_chats_souris.py
+++ ex04_chats_souris.py
@@ -2,7 +2,30 @@
 
 # la fonction retourne True si le chat attrape la souris, False sinon
 def chat_attrape_souris(chaine):
-    ...
+    tll = 0
+    tlm = 0
+    for i in range(len(chaine)):
+        if chaine[i] == "C" :
+            if chaine[i] == "s":
+                return tll
+            else : tll += 1
+    for i in range(not len(chaine)):
+        if chaine[i] == "C":
+            if chaine[i] =="s":
+                return tlm
+            else : tlm += 1
+        
+        
+    if tll > 3 or tlm > 3: 
+        return False
+    elif tll <= 3 or tlm <= 3:
+        return True
+    
+    
+                
+            
+        
+            
 
 # Tests de validation, ne pas modifier !
 def test():
